63d311fc85801e638d9066f5a10c2e7c49df24b1
Thomas Colomb <tcolomb@lafourchette.com>
2017-05-19 11:10:16 +0200

    Change some metrics in counter

--- start.sh
+++ start.sh
@@ -2,12 +2,8 @@
 
 set -e
 
-env
-
-sed -i.bak s/%host%:%port%/$REMOTE_HOSTPORT/g /opt/jmx_exporter/config.yml
-sed -i.bak s/%ssl%/$JMX_SSL/g /opt/jmx_exporter/config.yml
-
-cat /opt/jmx_exporter/config.yml
+sed -i s/%host%:%port%/$REMOTE_HOSTPORT/g /opt/jmx_exporter/config.yml
+sed -i s/%ssl%/$JMX_SSL/g /opt/jmx_exporter/config.yml
 
 SERVICE_PORT=9231
 JVM_OPTS="-Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.port=18983"
