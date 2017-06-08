669193042afd2185c554ce83e0d9c2d0c8d563d5
Thomas Colomb <colomb.thomas@free.fr>
2017-06-08 12:04:53 +0200

    Update release script

--- release.sh
+++ release.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+rm -rf .build .tarballs
+
 VERSION=$(cat VERSION)
 
 make build
@@ -9,9 +11,10 @@ promu checksum .tarballs
 promu release .tarballs
 
 
+rm prometheus-solr-exporter
 ln -s .build/linux-amd64/prometheus-solr-exporter prometheus-solr-exporter
 
-echo "make docker DOCKER_IMAGE_NAME=noony/prometheus-solr-exporter DOCKER_IMAGE_TAG=v$VERSION"
+make docker DOCKER_IMAGE_NAME=noony/prometheus-solr-exporter DOCKER_IMAGE_TAG=v$VERSION
 docker login
 docker tag "noony/prometheus-solr-exporter:v$VERSION" "noony/prometheus-solr-exporter:latest"
 docker push noony/prometheus-solr-exporter
